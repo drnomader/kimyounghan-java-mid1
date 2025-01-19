82b8081f27b2bfb3b4172acb0f259aed5404bfd7
drnomader <drnomader@gmail.com>
2025-01-19 23:14:45 +0900

    Add lang.string package

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class CharArrayMain {
+	public static void main(String[] args) {
+		char[] charArr = new char[]{'h', 'e', 'l', 'l', 'o'};
+		System.out.println(charArr);
+		System.out.println("charArr = " + charArr);
+
+		String str = "hello";
+		System.out.println("str = " + str);
+	}
+}
